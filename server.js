2887e3d1571875146a5552b1b047fc1fa35a7935
Helder Ferreira <helder.ferreira@pingpost.pt>
2018-08-10 14:49:33 +0100

    -> Installed and configured express-swagger-generator to document the API

--- server.js
+++ server.js
@@ -1,16 +1,55 @@
 const Joi = require('joi');
 const express = require('express');
 const app = express();
+const expressSwagger = require('express-swagger-generator')(app);
 const port = process.env.PORT || 3000;
 const printPedo = require('./peDoLogic');
 
+const options = {
+    swaggerDefinition: {
+        info: {
+            description: 'This is a simple node API',
+            title: 'Simple Node Api',
+            version: '1.0.0',
+        },
+        host: `localhost:${port}`,
+        basePath: '/v1',
+        produces: [
+            "application/json",
+            "application/xml"
+        ],
+        schemes: ['http', 'https'],
+        securityDefinitions: {
+            JWT: {
+                type: 'apiKey',
+                in: 'header',
+                name: 'Authorization',
+                description: "",
+            }
+        }
+    },
+    basedir: __dirname, //app absolute path
+    files: ['./server.js'] //Path to the API handle folder
+};
+
+expressSwagger(options);
+
+
 // Root Route
 app.get('/', (req, res) => {
-    res.send('Welcome to this simple node Api');
+    res.redirect('/api-docs');
 });
 
-
-app.get('/api/print', (req, res) => {
+/**
+ * This function comment is parsed by doctrine
+ * @route GET /api/print
+ * @group api - basic functionality of API
+ * @param {number} min.query - minimum value
+ * @param {number} max.query - maximum value
+ * @returns {object} 200 - An array of peDo values
+ * @returns {Error}  400 - Invalid parameters
+ */
+app.get('/v1/api/print', (req, res) => {
     const {
         query
     } = req;
@@ -25,11 +64,16 @@ app.get('/api/print', (req, res) => {
     const {error} = Joi.validate(rangeNums, schema);
 
     if (error) {
-        res.status(400).send(error.details[0].message);
+        res.status(400).send({error: error.details[0].message});
         return;
     }
 
     res.send(printPedo(rangeNums.min, rangeNums.max));
+});
+
+// Redirect page not found
+app.use(function (req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
 });
 
 console.log(`Listening on port ${port}...`);
